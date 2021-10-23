4597241064f3f85ef9322328ed5bbce3f33820be
dheerajrijhwani <dheerajrijhwani@gmail.com>
2021-10-23 16:14:54 +0530

    Complete Implementation

--- app/src/main/java/com/example/lottiecomposestarter/MainActivity.kt
+++ app/src/main/java/com/example/lottiecomposestarter/MainActivity.kt
@@ -3,16 +3,22 @@ package com.example.lottiecomposestarter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.LottieComposition
+import com.airbnb.lottie.LottieProperty
 import com.airbnb.lottie.compose.*
+import com.airbnb.lottie.compose.R
 import com.example.lottiecomposestarter.ui.theme.LottieComposeStarterTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,26 +34,64 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    @Composable
-    fun Loader() {
-        val compositionResult: LottieCompositionResult = rememberLottieComposition(
-            spec = LottieCompositionSpec.RawRes(R.raw.anim_truck)
-        )
+}
 
-        val progress by animateLottieCompositionAsState(
-            composition = compositionResult.value,
-            isPlaying = true,
-            iterations = LottieConstants.IterateForever,
-            speed = 0.5f
-        )
+@Composable
+private fun Loader() {
 
-        LottieAnimation(
-            composition = compositionResult.value,
-            progress = progress,
-            modifier = Modifier.padding(all = 20.dp)
-        )
 
-    }
+    val compositionResult: LottieCompositionResult =
+        rememberLottieComposition(LottieCompositionSpec.Asset("lottie/navigate.json"))
+
+    val progress by animateLottieCompositionAsState(
+        compositionResult.value,
+        isPlaying = true,
+        iterations = LottieConstants.IterateForever,
+        speed = 1.0f
+    )
+
+    val color by derivedStateOf { Color.Red }
+
+    val dynamicProperties = rememberLottieDynamicProperties(
+        rememberLottieDynamicProperty(
+            property = LottieProperty.COLOR,
+            value = color.toArgb(), keyPath = arrayOf(
+                "compass needle",
+                "Shape 1",
+                "Fill 1"
+            )
+        ),
+
+        rememberLottieDynamicProperty(
+            property = LottieProperty.COLOR,
+            value = color.toArgb(), keyPath = arrayOf(
+                "donut",
+                "Group 1",
+                "Fill 1"
+            )
+        ),
+        rememberLottieDynamicProperty(
+            property = LottieProperty.OPACITY,
+            value = 50,
+            keyPath = arrayOf(
+                "compass needle",
+                "Shape 1",
+                "Fill 1"
+            )
+        ),
+    )
+
+    LottieAnimation(
+        compositionResult.value,
+        progress,
+        dynamicProperties = dynamicProperties,
+        modifier = Modifier.padding(all = 50.dp)
+    )
 
 }
+
+
+
+
+
 
